21926d08b2dcf6058e0f32b7b185087ec1fdcff3
Michael J. Bondra <mjbondra@gmail.com>
2023-04-30 08:39:33 -0700

    remote extraneous use of pre element

--- src/core/components/version-stamp.jsx
+++ src/core/components/version-stamp.jsx
@@ -2,7 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const VersionStamp = ({ version }) => {
-  return <small><pre className="version"> { version } </pre></small>
+  // A11Y NOTE: [SI] Accessibility best practices
+  // - use <div> instead of <pre> for text that is not code or figure
+  return <small><div className="preformatted-text version"> { version } </div></small>
 }
 
 VersionStamp.propTypes = {
